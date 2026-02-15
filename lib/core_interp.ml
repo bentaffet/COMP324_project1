bd4fa29633e427db1dcb3f9822c416301fa3c292
johnkenjikono <johnk.kono@gmail.com>
2026-02-15 16:34:12 -0500

    Finished adding bool match cases to binop and unop. Created expression cases for Bool and Unop

--- lib/core_interp.ml
+++ lib/core_interp.ml
@@ -66,38 +66,101 @@ end
  *)
 (*! binop header !*)
 let binop (op : E.binop) (v : Value.t) (v' : Value.t) : Value.t =
-
   match (op, v, v') with
-(*! end !*)
-(*! binop plus !*)
+
   | (E.Plus, Value.V_Int n, Value.V_Int n') -> Value.V_Int (n + n')
-(*! end !*)
   | (E.Minus, Value.V_Int n, Value.V_Int n') -> Value.V_Int (n - n')
   | (E.Times, Value.V_Int n, Value.V_Int n') -> Value.V_Int (n * n')
   | (E.Div, Value.V_Int n, Value.V_Int n') -> Value.V_Int (n / n')
+  | (E.Mod, Value.V_Int n, Value.V_Int n') -> Value.V_Int()
+  | (E.And, Value.V_Int n, Value.V_Int n') -> failwith "typeerror"
+  | (E.Or, Value.V_Int n, Value.V_Int n') -> failwith "typeerror"
+  | (E.Eq, Value.V_Int n, Value.V_Int n') -> Value.V.Bool(n=n')
+  | (E.Ne, Value.V_Int n, Value.V_Int n') -> Value.V.Bool(n not = n')
+  | (E.Lt, Value.V_Int n, Value.V_Int n') -> Value.V.Bool(n < n')
+  | (E.Le, Value.V_Int n, Value.V_Int n') -> Value.V.Bool(n <= n')
+  | (E.Gt, Value.V_Int n, Value.V_Int n') -> Value.V.Bool(n > n')
+  | (E.Ge, Value.V_Int n, Value.V_Int n') -> Value.V.Bool(n >= n')
+  | (E.Plus, Value.V.Bool b, Value.V.Bool b') -> failwith "typeerror"
+  | (E.Minus, Value.V.Bool b, Value.V.Bool b') -> failwith "typeerror"
+  | (E.Times, Value.V.Bool b, Value.V.Bool b') -> failwith "typeerror"
+  | (E.Div, Value.V.Bool b, Value.V.Bool b') -> failwith "typeerror"
+  | (E.Mod, Value.V.Bool b, Value.V.Bool b') -> failwith "typeerror"
+  | (E.And, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b & b')
+  | (E.Or, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b or b')
+  | (E.Eq, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b = b)
+  | (E.Ne, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b not = b')
   | _ -> failwith "Unimplemented"
+
+  (*
+  | (E.Lt, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b < b')
+  | (E.Le, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b <= n')
+  | (E.Gt, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b > b')
+  | (E.Ge, Value.V.Bool b, Value.V.Bool b') -> Value.V.Bool(b >= b')
+
+  | (E.Plus, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Minus, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Times, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Div, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Mod, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.And, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Or, Value.V_Int n, Value.V.Bool b) ->
+  | (E.Eq, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Ne, Value.V_Int n, Value.V.Bool b) ->
+  | (E.Lt, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Le, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Gt, Value.V_Int n, Value.V.Bool b) -> 
+  | (E.Ge, Value.V_Int n, Value.V.Bool b) -> 
+*)
+
+let unop(sign: E.unop) (v: Value.t): Value.t =
+  match (sign, v) with
+  | (E.Neg, Value.V_Int n) -> Value.V_Int(-n)
+  | (E.Neg, Value.V_Bool b) -> failwith "typeerror"
+  | (E.Not, Value.V_Bool b) -> Value.V_Bool(not b)
+  | (E.Not, Value.V_Int n) -> failwith "typeerror"
+  
+let if(v:Value.t) (v0:Value.t) (v1:Value.t): Value.t =
+  match (v,v0,v1) with
+  | (Value.V_Bool b = true, , ) ->
+  | (Value.V_Bool) b = false, , ) ->
+
+  (*NEED TO FINISH IMPLEMEENTING*)
+
+
 
 
 let rec exec(rho:Env.t) (e: E.t) : Value.t =
   match e with
 (*! end !*)
   | E.Var x -> Env.lookup rho x
+
   | E.Num n -> Value.V_Int n
-  (*| E.Neg  ->
-    let V_Int n = exec rho e in
-    V_Int (-n)*)
-(*! eval binop !*)
+
+  | E.Bool (b) -> Value.V_Bool b
+
+  | E.Unop (sign,e) -> 
+    let v=exec rho e in
+    unop sign v
+
   | E.Binop (op, e, e') ->
     let v = exec rho e in
     let v' = exec rho e' in
     binop op v v'
-(*! end !*)
-(*! eval let !*)
+
+  | E.If(e,e0,e1) -> 
+    let v=exec rho e in
+    let v0=exec rho e0 in
+    let v1=exec rho e1 in
+    if v v0 v1
+
   | E.Let (x, e', e) ->
     let v' = exec rho e' in
     exec (Env.update rho x v') e
-  | E.Call (x, e) -> e
-    
+
+  | E.Call (x, e) -> 
+    (*NEED TO IMPLEMEENT*)
+  
 
 
 (* exec p = v, where `v` is the result of executing `p`.
